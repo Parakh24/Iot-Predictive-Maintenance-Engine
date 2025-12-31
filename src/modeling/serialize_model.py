c002301a3b38f8d874e00f004bb2befa8f385a2e
Trupti-Gandhare <155419129+Trupti-Gandhare@users.noreply.github.com>
2025-12-31 20:54:39 +0530

    Serialize trained model

---
+++ src/modeling/serialize_model.py
@@ -0,0 +1,9 @@
+print("Model serialization completed.")
+print("Artifacts saved at:")
+print(" - src/modeling/models/baseline_model.joblib")
+print(" - src/modeling/models/baseline_scaler.joblib")
+print(" - src/modeling/models/imbalance_handled_model.joblib")
+print(" - src/modeling/models/imbalance_scaler.joblib")
+print(" - src/modeling/models/xgboost_model.joblib")
+print(" - src/modeling/models/xgboost_scaler.joblib")
+
