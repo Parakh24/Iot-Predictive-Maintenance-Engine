8efd6b452f2c89a92c741cc9b5972eb215d6fb46
Parakh24 <parakhvirnawe2404@gmail.com>
2026-01-04 16:55:19 +0530

    src/api/app.py is optimized by changing the arguments of predict failure and app.run(debug=True) to app.run(debug=False)

--- src/api/app.py
+++ src/api/app.py
@@ -1,7 +1,12 @@
 from flask import Flask, request, jsonify
+import joblib
 from inference import predict_failure
 
 app = Flask(__name__)
+
+# Load model artifacts ONCE at startup
+model = joblib.load("models/final_xgb_model.joblib")
+preprocessor = joblib.load("models/preprocessing_pipeline.joblib")
 
 # -------------------------------------------------
 # Health check
@@ -24,7 +29,7 @@ def predict():
             if field not in data:
                 return jsonify({"error": f"Missing field: {field}"}), 400
 
-        result = predict_failure(data)
+        result = predict_failure(data, model, preprocessor)                            #before only data as arguments after, optimization model and preprocessor
 
         prob = result["failure_probability"]
         risk = "HIGH" if prob > 0.7 else "MEDIUM" if prob > 0.4 else "LOW"
@@ -40,4 +45,7 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)                           #before True but after optimization false is there 
+
+
+    #what i have done is big latency improvement work done 
